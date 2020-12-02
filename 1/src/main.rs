725f7cd4fa1a389f633b1b63ace88708db702c0a
Kristoffer Grönlund <krig@koru.se>
2020-12-02 16:06:13 +0100

    day 1

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,4 +1,18 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
-    
+    let mut stdin = io::stdin();
+    let mut buffer = String::new();
+    stdin.read_to_string(&mut buffer).expect("input");
+
+    let v: Vec<i32> = buffer.lines().map(|line| line.trim().parse().expect("not a number")).collect();
+
+    for (pa, a) in v.iter().enumerate() {
+        for b in v.iter().skip(pa + 1) {
+            if a + b == 2020 {
+                println!("{} * {} = {}", a, b, a * b);
+                return;
+            }
+        }
+    }
 }
