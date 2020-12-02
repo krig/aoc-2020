1b2c1955104895616828ce817832832314cddc22
Kristoffer Grönlund <krig@koru.se>
2020-12-02 16:15:19 +0100

    day 1 part 2

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -5,13 +5,15 @@ fn main() {
     let mut buffer = String::new();
     stdin.read_to_string(&mut buffer).expect("input");
 
-    let v: Vec<i32> = buffer.lines().map(|line| line.trim().parse().expect("not a number")).collect();
+    let v: Vec<u64> = buffer.lines().map(|line| line.trim().parse().expect("not a number")).collect();
 
     for (pa, a) in v.iter().enumerate() {
-        for b in v.iter().skip(pa + 1) {
-            if a + b == 2020 {
-                println!("{} * {} = {}", a, b, a * b);
-                return;
+        for (pb, b) in v.iter().skip(pa + 1).enumerate() {
+            for c in v.iter().skip(pb + 1) {
+                if a + b + c == 2020 {
+                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
+                    return;
+                }
             }
         }
     }
