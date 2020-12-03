3e0bd65e06b5d680598484be0241722805f4dbde
Kristoffer Grönlund <krig@koru.se>
2020-12-03 18:41:25 +0100

    day 3: use fold

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -5,18 +5,17 @@ fn main() {
     io::stdin().read_to_string(&mut input).expect("input");
 
     let map: Vec<&str> = input.lines().collect();
-    let mapwidth: usize = map[0].trim().chars().count();
+    let width = map[0].trim().chars().count();
     let slopes: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let mut total: usize = 1;
-    for slope in slopes.iter() {
+    let total = slopes.iter().fold(1, |acc, slope| {
         let mut trees: usize = 0;
         let mut pos: (usize, usize) = (0, 0);
         loop {
-            pos = ((pos.0 + slope.0) % mapwidth, pos.1 + slope.1);
+            pos = ((pos.0 + slope.0) % width, pos.1 + slope.1);
             if pos.1 >= map.len() { break; }
             if map[pos.1].chars().nth(pos.0).unwrap() == '#' { trees += 1; }
         }
-        total *= trees;
-    }
+        acc * trees
+    });
     println!("{}", total);
 }
