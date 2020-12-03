33dcdd927b23b7dbc02f744058e6ad40a81f5927
Kristoffer Grönlund <krig@koru.se>
2020-12-03 18:27:08 +0100

    cleanup pass day 3

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,40 +1,22 @@
 use std::io::{self, Read};
 
 fn main() {
-    let mut stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_to_string(&mut input).expect("input");
+    io::stdin().read_to_string(&mut input).expect("input");
 
     let map: Vec<&str> = input.lines().collect();
     let mapwidth: usize = map[0].trim().chars().count();
-    let slopes: [(usize, usize); 5] = [
-        (1, 1),
-        (3, 1),
-        (5, 1),
-        (7, 1),
-        (1, 2),
-    ];
-    let mut treemul: usize = 1;
+    let slopes: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut total: usize = 1;
     for slope in slopes.iter() {
         let mut trees: usize = 0;
         let mut pos: (usize, usize) = (0, 0);
         loop {
-            pos.0 += slope.0;
-            pos.1 += slope.1;
-
-            while pos.0 >= mapwidth {
-               pos.0 -= mapwidth;
-            }
-
-            if pos.1 >= map.len() {
-                break;
-            }
-
-            if map[pos.1].chars().nth(pos.0).unwrap() == '#' {
-                trees += 1;
-            }
+            pos = ((pos.0 + slope.0) % mapwidth, pos.1 + slope.1);
+            if pos.1 >= map.len() { break; }
+            if map[pos.1].chars().nth(pos.0).unwrap() == '#' { trees += 1; }
         }
-        treemul *= trees;
+        total *= trees;
     }
-    println!("{}", treemul);
+    println!("{}", total);
 }
